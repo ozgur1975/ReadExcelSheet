13747fa7b874ff45fe1fde9fd40d3ee642937cce
Teknosol.Ozgur <ozgur@teknosol.com.tr>
2022-11-28 17:50:46 +0300

    excel düzenleme

--- ReadExcelSheet/Program.cs
+++ ReadExcelSheet/Program.cs
@@ -2,5 +2,6 @@
 using ReadExcelSheet;
 
 //Kombinasyon.Konbinasyonlar(new List<int>() { 1, 2, 3, 4, 5, 6, 7 },6);
-ExcelReadSheet.Read02(@"C:\Users\ozgur.yurtsever\OneDrive - teknosol.com.tr\Masaüstü\SLIStatistik.xlsx");
+//ExcelReadSheet.Read02(@"C:\Users\ozgur.yurtsever\OneDrive - teknosol.com.tr\Masaüstü\SLIStatistik.xlsx");
+ExcelReadSheet.Read02(@"..\..\..\..\SLIStatistik.xlsx");
 //ExcelReadSheet.Read01(@"C:\Users\ozgur.yurtsever\OneDrive - teknosol.com.tr\Masaüstü\SLIStatistik.xlsx");
